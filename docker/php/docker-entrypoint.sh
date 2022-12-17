8154691c686cf5266cc240ab2811b39ae770bcaa
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-12-17 19:08:22 +0100

    docker setup

---
+++ docker/php/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+	if [ "$BUILD_APP_ENV" = "dev" ]; then
+		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+		composer install --no-scripts --no-interaction --prefer-dist --optimize-autoloader
+	fi
+	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+fi
+
+exec docker-php-entrypoint "$@"
